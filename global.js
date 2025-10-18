6875af51e3a8d4d6c7ea7a873fdac270a9548fae
James Alan Wootton <jawootton@ucsd.edu>
2025-10-18 08:38:04 -0700

    test push

--- global.js
+++ global.js
@@ -3,3 +3,7 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Step 2.1: Get an array of all nav links
+// const navLinks = $$("nav a");
+// console.log(navLinks);
